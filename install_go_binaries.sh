c83f79baa022c49772c02c5fcfe32dc2b4c98766
Lasse Wolter <wolter.lasse@gmail.com>
2019-09-24 18:50:35 +0100

    Added install script to install go binaries

---
+++ install_go_binaries.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+katz_dir=$GOPATH/src/github.com/katzenpost
+
+# Install binaries for authority, server and catshadow client
+go install $katz_dir/daemons/authority/nonvoting
+go install $katz_dir/daemons/server
+go install $katz_dir/catshadow/cmd
+
+# For the following two go executables it's a bit more difficult:
+# we want to install the binaries under a name that's different to 
+# the directory the main.go file is in
+# Since go's install command has no such functionality we use
+# 'go build' and then move the binary to the correct folder
+go build -o spool_server_tmp $katz_dir/memspool/server
+mv $katz_dir/spool_server_tmp $GOPATH/bin/spool_server
+
+go build -o panda_tmp $katz_dir/panda/server
+mv $katz_dir/panda_tmp $GOPATH/bin/panda
+
