28e4e5ed161de746b4609101d9addc5bc1e4f37c
Lasse Wolter <wolter.lasse@gmail.com>
2019-11-13 11:13:56 +0000

    Minor fix in make.sh

--- install/make.sh
+++ install/make.sh
@@ -6,5 +6,5 @@ unzip dep.zip -d $GOPATH/src
 echo "Installing go binaries..."
 # Fixing path means that the path in the different config files is changed to match
 # the current system - otherwise the start.sh file won't work
-./scripts/fix_path.sh
+./scripts/fix_path_and_permissions.sh
 ./scripts/install_go_binaries.sh
