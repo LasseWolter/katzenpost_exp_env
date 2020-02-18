6171c35d214a2fd392a1dbe5d99b2ca9369e10e0
Lasse Wolter <wolter.lasse@gmail.com>
2020-02-18 15:44:39 +0100

    Fixed the fix_path_and_permissions.sh script
    + the path wasn't correct anymore so it wasn't matched by the sed
    command
    + added path to the config.toml on top of the mixnet directory

--- install/scripts/fix_path_and_permissions.sh
+++ install/scripts/fix_path_and_permissions.sh
@@ -4,8 +4,8 @@ NEW_PATH=$GOPATH
 MIXNET_PATH="$GOPATH/src/github.com/katzenpost/mixnet"
 
 # Chnange the path in the different config files to match the current system
-sed -i "s|/home/lasse/Programming/mix_net/|${NEW_PATH}/src/github.com/katzenpost/mixnet/|g" ${MIXNET_PATH}/*/*.toml
-sed -i "s|/home/lasse/Programming/go_projects/|$NEW_PATH/|g" ${MIXNET_PATH}/service_provider/service_provider.toml 
+sed -i "s|/home/lasse/go/src/github.com/katzenpost/mixnet/|${NEW_PATH}/src/github.com/katzenpost/mixnet/|g" ${MIXNET_PATH}/*/*.toml ${MIXNET_PATH}/*.toml
+sed -i "s|/home/lasse/go/|$NEW_PATH/|g" ${MIXNET_PATH}/service_provider/service_provider.toml 
 
 # Fix permissions - 700 is required such that the go binaries execute properly
 chmod -R 700 ${MIXNET_PATH}/{service_provider,provider,auth,mix*}
